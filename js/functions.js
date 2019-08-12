fe33f8b6897e5e7e74b276c62b0349ece7355a97
Jon Barson <jbarson@gmail.com>
2019-08-12 09:17:41 -0400

    adding 3Dmap

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,25 @@
-//Thanks to http://speckyboy.com/2012/03/07/scroll-to-internal-link-with-jquery/
-// function scrollToDiv(element,navheight){
-//     var offset = element.offset();
-//     var offsetTop = offset.top;
-//     var totalScroll = offsetTop-navheight;
-//     $('body').animate({
-//             scrollTop: totalScroll
-//     }, 500);
-// }
-// $('.slownav').click(function(){
-//     var el = $(this).attr('href');
-//     var elWrapped = $(el);
-//     scrollToDiv(elWrapped,40);
-//     return false;
-// });
+// Thanks to http://speckyboy.com/2012/03/07/scroll-to-internal-link-with-jquery/
+/**
+ *
+ * @param {*} element the jQuery element you are scrolling to
+ * @param {number} navheight Offset in pixels to center the element
+ */
+import $ from 'jquery'
+
+function scrollToDiv (element, navheight) {
+  var offset = element.offset()
+  var offsetTop = offset.top
+  var totalScroll = offsetTop - navheight
+  $('body').animate(
+    {
+      scrollTop: totalScroll
+    },
+    500
+  )
+}
+$('.slownav').click(function () {
+  var el = $(this).attr('href')
+  var elWrapped = $(el)
+  scrollToDiv(elWrapped, 40)
+  return false
+})
