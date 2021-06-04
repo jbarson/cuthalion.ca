c93a8d81bb65612fc450acf507ba002336821e2e
Jon Barson <jbarson@gmail.com>
2021-06-04 12:45:01 -0400

    minimal site done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/sidebar/Sidebar';
+import About from './components/about/About';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="side">
+          <nav className="navbar side" >
+            <Sidebar />
+          </nav>
+        </div>
+        <div className="main">
+          <About />
+        </div>
+      </div>
+    </Router>
   );
 }
 
