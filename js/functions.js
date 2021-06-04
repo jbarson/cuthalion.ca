aac9972b21cbeaa0787fe0ad778c7f4bda2ae557
Jon Barson <jbarson@gmail.com>
2021-06-04 10:04:14 -0400

    barebones

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-const bodyEle = document.querySelector('body')
-const anchorList = document.querySelectorAll('.slownav')
-anchorList.forEach(element => {
-  element.addEventListener('click', e => {
-    e.preventDefault()
-    const targetEle = document.querySelector(e.target.attributes.href.nodeValue)
-    targetEle.scrollIntoView({top: 300, behavior: 'smooth'})
-  })
-})
