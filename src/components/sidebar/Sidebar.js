20eba496cf7b137aa48a1de07e4d02d3aa0e0292
Jon Barson <Jon Barson>
2021-10-04 09:18:54 -0400

    fixing alt tag in sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './Sidebar.css';
 import { SiLinkedin, SiGithub, SiTwitter, SiGmail, SiInstagram } from 'react-icons/si';
 import logo from '../images/AngusTransShad.png'
 
-class Sidebar extends Component {
-  render() {
+function Sidebar() {
 
-    return (
-      <div className="sidebar">
-        <img src={logo} />
-        <h1>Jon Barson</h1>
-        <h2>Developer</h2>
-        <h2>Educator</h2>
-        <h2>Mentor</h2>
-        <div className="socials">
-          <a href="https://www.linkedin.com/in/jon-barson-b3a8467/" rel="opener noreferrer" target="_blank"><SiLinkedin /></a>
-          <a href="https://github.com/jbarson" rel="opener noreferrer" target="_blank" ><SiGithub /></a>
-          <a href="https://twitter.com/jbarson" rel="opener noreferrer" target="_blank" ><SiTwitter /></a>
-          <a href="https://www.instagram.com/jbarson/" rel="opener noreferrer" target="_blank" ><SiInstagram /></a>
-          <a href="mailto:jbarson@gmail.com" rel="opener noreferrer" target="_blank" ><SiGmail /></a>
-        </div>
-
+  return (
+    <div className="sidebar">
+      <img src={logo} alt='' />
+      <h1>Jon Barson</h1>
+      <h2>Developer</h2>
+      <h2>Educator</h2>
+      <h2>Mentor</h2>
+      <div className="socials">
+        <a href="https://www.linkedin.com/in/jon-barson-b3a8467/" rel="opener noreferrer" target="_blank"><SiLinkedin /></a>
+        <a href="https://github.com/jbarson" rel="opener noreferrer" target="_blank" ><SiGithub /></a>
+        <a href="https://twitter.com/jbarson" rel="opener noreferrer" target="_blank" ><SiTwitter /></a>
+        <a href="https://www.instagram.com/jbarson/" rel="opener noreferrer" target="_blank" ><SiInstagram /></a>
+        <a href="mailto:jbarson@gmail.com" rel="opener noreferrer" target="_blank" ><SiGmail /></a>
       </div>
-    )
-  }
+
+    </div>
+  )
+
 }
 
 export default Sidebar
